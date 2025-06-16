aa0c88f1e1bd247df2d9e331ba7839965b4a7ada
xDAGAx13 <rohandaga05@gmail.com>
2025-06-16 14:23:54 +0530

    Fixed all eas errors

--- app.config.js
+++ app.config.js
@@ -1,36 +1,59 @@
-// app.config.js
-import 'dotenv/config';
-
-export default {
-  expo: {
-    name: "outrack",
-    slug: "outrack",
-    "owner":"dagax13",
-      "updates": {
-    "url": "https://u.expo.dev/778641f5-8e2c-4c01-b818-834b2b8a3121"  
-  },
-  "runtimeVersion": {
-    "policy": "appVersion"
-  },
-  "android": {
-      "package": "com.dagax13.outrack"
+import 'dotenv/config'
+export default{
+  "expo": {
+    "name": "OUTRACK",
+    "slug": "OUTRACK",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/images/icon.png",
+    "scheme": "outrack",
+    "userInterfaceStyle": "automatic",
+    "newArchEnabled": true,
+    "ios": {
+      "supportsTablet": true
     },
-    extra: {
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/images/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "edgeToEdgeEnabled": true
+    },
+    "web": {
+      "bundler": "metro",
+      "output": "static",
+      "favicon": "./assets/images/favicon.png"
+    },
+    "plugins": [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          "image": "./assets/images/splash-icon.png",
+          "imageWidth": 200,
+          "resizeMode": "contain",
+          "backgroundColor": "#ffffff"
+        }
+      ]
+    ],
+    "experiments": {
+      "typedRoutes": true
+    },
+    "extra":{
+    "url": "https://u.expo.dev/778641f5-8e2c-4c01-b818-834b2b8a3121",  
       FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
       FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
       FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
       FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
       FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
       FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
-      OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-      "eas":{
-        "projectId":"778641f5-8e2c-4c01-b818-834b2b8a3121"
-      }
-      
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY,      
+       
     },
-  },
-  
-  
-
-
-};
+    "owner":"dagax13",
+  "runtimeVersion": {
+    "policy": "appVersion"
+  }
+    
+  }
+}
